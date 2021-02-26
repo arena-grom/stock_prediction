b4f06963915f9a49a5858a1659d6cbdf295f1254
arena-grom <arisha.gr@gmail.com>
2021-02-26 15:43:10 +0100

    training and plotting loss lstm

--- lstm_stock_prediction.py
+++ lstm_stock_prediction.py
@@ -9,7 +9,7 @@ import pandas as pd
 #import time
 #from datetime import datetime
 import matplotlib.pyplot as plt
-from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+#from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM #, SimpleRNN
@@ -19,57 +19,47 @@ from keras.layers import Dense, Dropout, LSTM #, SimpleRNN
 
 #%% Functions
 
-def plot_stock_data(data, title, feature='Close' , date_formatter='%Y', color='darkred'):
+def plot_stock_data(df, title, feature='Close' , date_formatter='%Y', color='darkred'):
     '''Function for plotting stock data'''
-    fig, ax = plt.subplots()
-    ax.plot(data['Date'], data[feature], color=color)
-    #
     
-    if date_formatter == '%Y':
-        ax.xaxis.set_major_formatter(DateFormatter(date_formatter))
-        ax.xaxis.set_major_locator(plt.MaxNLocator(20))
-    elif date_formatter == '%Y-%m':
-        ax.xaxis.set_major_formatter(DateFormatter(date_formatter))
-        ax.xaxis.set_major_locator(YearLocator())
-        ax.xaxis.set_minor_locator(MonthLocator())
-    else:
-        ax.xaxis.set_major_locator(plt.MaxNLocator(20))
-
-    plt.suptitle(title)
-    plt.xticks(rotation=45)
+    df[feature].plot(figsize=(14,7), color=color)
+    plt.suptitle(title, fontsize=18)
+    # plt.xticks(rotation=45)
     plt.xlabel('Date')
     plt.ylabel(feature)
     plt.grid()
     plt.show()
     
     
-def preprocess_data(data, feature='Close', seq_len=60):
+def preprocess_data(data, feature='Close', seq_len=60, test_n=600):
     """ Function that transforms raw data to LSTM input data """
     
-    
-    df = data[['Date', feature]]
-    df.set_index('Date', inplace=True)
-    
-    train_data = df.iloc[:-600,:]
-    test_data = df.iloc[-600:,:]
+    df = data[[feature]]
+    print(f'Input data shape: {df.shape}\nExtracting test set.')
+    # df.reset_index(drop=True, inplace=True)
+    #df.Date = pd.to_datetime(df.Date, format="%Y-%m-%d")
+    train_data = df.iloc[:-test_n,:]
+    test_data = df.iloc[-test_n:,:]
     
     # scale training dataset
     scaler = MinMaxScaler()
     train_scaled = scaler.fit_transform(train_data)
-    print(f'Scaled data:\n{train_scaled}\nShape: {train_scaled.shape}')
+    print(f'\nScaled training data:\n{train_scaled}\nShape: {train_scaled.shape}')
     
-    seq_len=60
+    # create sequences of training and predictors
     length=len(train_data)
     X_train, y_train = [], []
     for i in range(seq_len, length):
-        
         X_train.append(train_scaled[i-seq_len:i, 0])
         y_train.append(train_scaled[i, 0])
 
-    # reshape
+    # reshape for LSTM
     X_train, y_train = np.array(X_train), np.array(y_train)
+    print(f'X_train:\n{X_train}\nShape: {X_train.shape}')
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-    print(f'\nShape of X_train: {X_train.shape}, y_train: {y_train.shape}')
+    print(f'\nNew shape of training data: X_train {X_train.shape}, y_train {y_train.shape}')
+    
+    # TODO reshape test_data
     
     return X_train, y_train, test_data
     
@@ -84,7 +74,7 @@ def evaluate_model():
     pass
 
 
-#%% Building LSTM architecture
+#%% Class LSTM architecture
 
 class StockLSTM():
     """ Class that sets up a LSTM network for Stock price prediction """
@@ -93,7 +83,8 @@ class StockLSTM():
         print('\nInitializing LSTM network.\n')
         self.X = X_train
         self.y = y_train
-        self.train_history=None
+        self.trained=None
+        self.compliled=False
         self.preds=None
         
         # initialize Keras MLP model and first input layer
@@ -112,14 +103,33 @@ class StockLSTM():
         
     def add_compile(self, loss='mean_squared_error', optimizer='adam'):
         self.model.compile(loss=loss, optimizer=optimizer)
+        self.compiled=True
         print(self.model.summary())
         
     """ Training the LSTM model on X_train """
-    def train(self, epochs=100, batch_size=100, validation_rate=0.3):
-        print('\nTraining MLP classifier on training data.')
-        self.train_history = self.model.fit(self.X, self.y, 
-                                            validation_split=validation_rate, 
-                                            epochs=epochs, batch_size=batch_size)
+    def train(self, epochs=100, batch_size=100, validation_rate=0.3, verbose=1):
+        if self.compiled:
+            print('\nTraining LSTM model on training data.')
+            self.trained = self.model.fit(self.X, self.y, 
+                                          validation_split=validation_rate, 
+                                          epochs=epochs, batch_size=batch_size,
+                                          verbose=verbose)
+        else: print('Error: Model not compiled!') 
+        
+    """ Plotting training """
+    def plot_history(self):
+        if self.trained != None:
+            fig = plt.figure(figsize=(14,7))
+            plt.plot(self.trained.history['loss'])
+            plt.plot(self.trained.history['val_loss'])
+            plt.suptitle('LSTM Loss during epochs', fontsize=18)
+            plt.ylabel('Loss')
+            plt.xlabel('Epoch')
+            plt.legend(['train', 'val'], loc='upper right')
+            plt.show()
+        
+        else: print('Error: Model not trained!') 
+        
         
     """ Predicting test data and evaluating the model """
     
@@ -138,35 +148,46 @@ class StockLSTM():
 
 
 nvidia_df = pd.read_csv('NVDA.csv')
+nvidia_df.set_index('Date', drop=False, inplace=True)
 print('Data read.')
 print(nvidia_df.shape)
-print(nvidia_df.describe())
+print(f'\nDataset:\n{nvidia_df.head()}')
+print(f'\nSummary:\n{nvidia_df.describe()}')
 
-print(f'NaNs in the data: {nvidia_df.isnull().sum()}')
+print(f'\nNaNs in the data: {nvidia_df.isnull().sum()}')
 
 print('# ------------------------------- #')
+#%% Plot
 
-plot_stock_data(data=nvidia_df, title='NVIDIA stock price all time') # date_formatter : ['%Y-%m-%d']
-plot_stock_data(data=nvidia_df.tail(1000), title='NVIDIA stock price 2017-2021', date_formatter='%Y-%m')
+plot_stock_data(nvidia_df, title='NVIDIA stock price all time') # date_formatter : ['%Y-%m-%d']
+plot_stock_data(nvidia_df.tail(1000), title='NVIDIA stock price 2017-2021', date_formatter='%Y-%m')
 
-# predict the opening price
+print('# ------------------------------- #')
+#%% Process the training data
 X_train, y_train, test = preprocess_data(nvidia_df)
 
 print('# ------------------------------- #')
+#%% Setting up LSTM 1
 
-#%% Setting up LSTM
+lstm_model = StockLSTM(X_train, y_train) # Init and first layer
+lstm_model.add_dropout() 
 
-lstm_model = StockLSTM(X_train, y_train, input_units=35) 
+lstm_model.add_lstm_layer(return_seq=True) # Second layer
 lstm_model.add_dropout() 
-lstm_model.add_lstm_layer(units=35) # last LSTM layer does not return seq
-lstm_model.add_dropout() 
-lstm_model.add_dense_layer()
+
+#lstm_model.add_lstm_layer(units=92) # last LSTM layer does not return seq
+#lstm_model.add_dropout()
+
+lstm_model.add_dense_layer() # Output layer
+
+# Compiling and training
 lstm_model.add_compile()
 
-lstm_model.train(epochs=20, batch_size=100)
+#%%
+lstm_model.train(epochs=10, batch_size=500)
+lstm_model.plot_history()
 
-# training MLP classifier 
-# clf.train(X_val=X_test, y_val=y_test)
+
 # predicted_y = clf.test_predict(X_test)
 
 # evaluate_model(y=y_test_raw, pred=predicted_y)
